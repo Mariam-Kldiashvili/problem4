1050a38ffce4bfac1c4c8728a953f3059b078fa7
Mariam-Kldiashvili <123800954+Mariam-Kldiashvili@users.noreply.github.com>
2023-04-04 21:53:34 +0400

    add

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // problem 1
 // შექმენით ფუნქცია რომელიც დააბრუნებს რიცხვი კენტია თუ ლუწი
 
-function showMessage(answer) {
-    if (answer % 2) { console.log("odd") }
-    else { console.log("even") };
-}
+// function showMessage(answer) {
+//     if (answer % 2) { console.log("odd") }
+//     else { console.log("even") };
+// }
 
-let answer = Number(prompt("Please enter the Number"));
-showMessage(answer);
+// let answer = Number(prompt("Please enter the Number"));
+// showMessage(answer);
 
 
 // problem2
@@ -19,16 +19,16 @@ showMessage(answer);
 
 
 
-let value;
+// let value;
 
-while (true) {
-    value = prompt("Enter your name", "");
-    if (value) break;
-}
+// while (true) {
+//     value = prompt("Enter your name", "");
+//     if (value) break;
+// }
 
-alert("your name is: " + value);
+// alert("your name is: " + value);
 
-console.log(value);
+// console.log(value);
 
 
 
@@ -36,25 +36,23 @@ console.log(value);
 // problem 3
 // კონსოლში დაბეჭდეთ 1-100 მდე ყველა ლუწი რიცხვი
 
-let i = 0;
-while (i < 100) {
-    i += 2;
-    console.log(i)
-};
+// let i = 0;
+// while (i < 100) {
+//     i += 2;
+//     console.log(i)
+// };
 
 
 
 // problem4
 // მომხმარებელს მოთხოვეთ რომ შეიყვანოს რიცხვები მანამ სანამ არ შეიყვანს უარყოფით რიცხვს
 
-let number;
+// let number;
 
-while(number>=0){
-    number = prompt("Enetr the number", "");
-    if (number) break;
-}
-alert ("numer is: " + number);
-console.log(number);
+// while (true) {
+//    let number = prompt("Enetr the number", "");
+//     if (number<0) break;
+// }
 
 
 
@@ -68,22 +66,39 @@ console.log(number);
 // პარამეტრად მიღებულ მასივში მოძებნის ყველა ელემენტს რომელიც იყოფა 5-ზე უნაშთოდ და ჩაწერს result მასივში.
 
 
+// let numbers = [10, 12, 42, 55, 100, 90, 32, 55];
+
+
+// function arrayNumbers(array) {
+//     let result = [];
+//     for( const elements of array)  {
+//         if (elements%5 == 0){
+//             result.push(elements)
+//         }
+//     }
+//     return result
+// }
+
+// console.log(arrayNumbers(numbers));
+
+
+
 
 // problem 6
 // let names = ["Gio","Luka","Nika","Ani","Eka","Nini","Sopo"];
 // კონსოლში დაბეჭდეთ names მასივში არსებული ყველა ელემენტი, გამოტოვეთ მხოლოდ ნიკა.
 // ლუპის საშუალებით.
 
-let names = ["Gio", "Luka", "Nika", "Ani", "Eka", "Nini", "Sopo"];
+// let names = ["Gio", "Luka", "Nika", "Ani", "Eka", "Nini", "Sopo"];
 
-for (let i = 0; i < names.length; i++) {
+// for (let i = 0; i < names.length; i++) {
 
-    if (i == 2) {
-        continue;
-    }
+//     if (names[i]=="Nika") 
+//         continue;
+    
 
-    console.log(names[i])
-};
+//     console.log(names[i])
+// };
 
 
 
@@ -95,6 +110,65 @@ for (let i = 0; i < names.length; i++) {
 // თუ ფუნქციაში გადაცემულ პარამეტრში არ იქნებ მინიმუმ 5 მომხმარებელი დააბრუნეთ წინადადება: "მასივში აუცილებლად უნდა იყოს მინიმუმ 5 მომხმარებელი"
 // თუ მინიმუმ 5 მომხმარებელი იქნება დააბრუნეთ თითოეული მათგანის სრული სახელი, გვარი და ასაკი.
 // (array.length გამოიყენეთ იმისთვის რომ გაიგოთ არის თუ არა მასივში შესაბამისი რაოდენობის ელემენტი)
+
+
+
+
+function usersInfo(info){
+
+    if(info.length < 5){
+        return "მასივში აუცილებლად უნდა იყოს მინიმუმ 5 მომხმარებელი"
+    }
+    
+    let userArray = [];
+
+    for (const user of info) {
+     userArray.push(user.userName + " " + user.age  + " " + user.surName);
+
+    }
+    return userArray
+    
+}
+
+
+
+const user1 = {
+    userName: "Jhon",
+    age: 22,
+    surName:"doe"
+}
+
+const user2 = {
+    userName: "Jhane",
+    age: 22,
+    surName: "doe"
+}
+
+const user3 = {
+    userName: "Jim",
+    age: 22,
+    surName: "doe"
+}
+
+const user4 = {
+    userName: "Luka",
+    age: 22,
+    surName: "doe"
+}
+
+const user5 = {
+    userName: "Nika",
+    age: 22,
+    surName: "doe"
+}
+
+const users = [user1, user2, user3, user4, user5];
+
+
+
+console.log(usersInfo(users));
+
+
 
 // problem8
 
